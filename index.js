d0b91f9a95737adac61ec06d5bb35b3a36f5bb17
ksha1018@naver.com <ksha1018@naver.com>
2018-02-26 11:46:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(string.toLowerCase() === string) {
-    console.log("I can\'t hear you!")
+    return "I can\'t hear you!"
   }
   
 }
