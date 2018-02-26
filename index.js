0146de3ae7bad9546808cceef13b76c72dcb60a0
ksha1018@naver.com <ksha1018@naver.com>
2018-02-26 11:43:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO, ${string}.toUpperCase()')
+}
+
+function logWhisper(string) {
+  console.log('hello, ${string).toLowerCase()')
+}
+
+function sayHiToGrandma(string) {
+  if(string === string.to)
+}
+
