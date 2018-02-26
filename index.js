4fa514799ce5eca190a0338770d78a4906b89e6a
ksha1018@naver.com <ksha1018@naver.com>
2018-02-26 11:50:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function sayHiToGrandma(string) {
   if(string.toUpperCase() === string) {
     return "YES INDEED!"
   }
-  if(string === "I love you Granma!") {
-    return "I love you too!"
+  if(string === "I love you, Granma.") {
+    return "I love you, too."
   }
 }
 
